403e909c7d60d728ad1a5a363a130bbc98eb7f69
rudra <bhattrudra17072005@gmail.com>
2025-02-08 20:44:52 -0500

    Added the functions for reset, update, GetSendRate and main implementation

--- ReliableUDP.cpp
+++ ReliableUDP.cpp
@@ -246,3 +246,270 @@ public:
 		printf("flow control initialized\n");
 		Reset();
 	}
+
+
+	//
+	// FUNCTION : Reset
+	// DESCRIPTION :
+	// This function resets the flow control system.  
+	// It sets the starting values for controlling the data speed.  
+	// PARAMETERS :
+	// None  
+	// RETURNS :
+	// void : Resets the internal values to default.
+	//
+
+	void Reset()
+	{
+		mode = Bad;
+		penalty_time = 4.0f;
+		good_conditions_time = 0.0f;
+		penalty_reduction_accumulator = 0.0f;
+	}
+
+	//
+// FUNCTION : Update
+// DESCRIPTION :
+// This function updates the flow control system based on network speed.  
+// It changes the sending speed to avoid network problems.  
+// PARAMETERS :
+// float deltaTime : Time since the last update.  
+// float rtt : Current round-trip time in milliseconds.  
+// RETURNS :
+// void : Updates the internal values for data sending speed.
+//
+
+	void Update(float deltaTime, float rtt)
+	{
+		const float RTT_Threshold = 250.0f;
+		if (mode == Good)
+		{
+			if (rtt > RTT_Threshold)
+			{
+				printf("* dropping to bad mode *\n");
+				mode = Bad;
+				if (good_conditions_time < 10.0f && penalty_time < 60.0f)
+				{
+					penalty_time *= 2.0f;
+					if (penalty_time > 60.0f)
+						penalty_time = 60.0f;
+					printf("penalty time increased to %.1f\n", penalty_time);
+				}
+				good_conditions_time = 0.0f;
+				penalty_reduction_accumulator = 0.0f;
+				return;
+			}
+			good_conditions_time += deltaTime;
+			penalty_reduction_accumulator += deltaTime;
+			if (penalty_reduction_accumulator > 10.0f && penalty_time > 1.0f)
+			{
+				penalty_time /= 2.0f;
+				if (penalty_time < 1.0f)
+					penalty_time = 1.0f;
+				printf("penalty time reduced to %.1f\n", penalty_time);
+				penalty_reduction_accumulator = 0.0f;
+			}
+		}
+		if (mode == Bad)
+		{
+			if (rtt <= RTT_Threshold)
+				good_conditions_time += deltaTime;
+			else
+				good_conditions_time = 0.0f;
+			if (good_conditions_time > penalty_time)
+				if (good_conditions_time > penalty_time)
+				{
+					printf("* upgrading to good mode *\n");
+					good_conditions_time = 0.0f;
+					penalty_reduction_accumulator = 0.0f;
+					mode = Good;
+					return;
+				}
+		}
+	}
+
+	//
+// FUNCTION : GetSendRate
+// DESCRIPTION :
+// This function returns the current speed of sending data.  
+// PARAMETERS :
+// None  
+// RETURNS :
+// float : The speed of sending data in packets per second.
+//
+
+	float GetSendRate()
+	{
+		return mode == Good ? 30.0f : 10.0f;
+	}
+private:
+	enum Mode
+	{
+		Good,
+		Bad
+	};
+	Mode mode;
+	float penalty_time;
+	float good_conditions_time;
+	float penalty_reduction_accumulator;
+};
+
+
+//
+// FUNCTION : main
+// DESCRIPTION :
+// This is the main function. It starts the UDP connection,  
+// controls client-server communication, and manages file transfer.  
+// PARAMETERS :
+// int argc : Number of command-line arguments.  
+// char* argv[] : Command-line arguments, such as the server IP address.  
+// RETURNS :
+// int : Returns 0 if the program runs successfully, otherwise returns an error code.
+//
+
+int main(int argc, char* argv[])
+{
+	// parse command line
+	enum Mode
+	{
+		Client,
+		Server
+	};
+	Mode mode = Server;
+	Address address;
+	char filename[256] = { 0 };
+	if (argc >= 2)
+	{
+		int a, b, c, d;
+#pragma warning(suppress : 4996) 
+		if (sscanf(argv[1], "%d.%d.%d.%d", &a, &b, &c, &d))
+		{
+			mode = Client;
+			address = Address(a, b, c, d, ServerPort);
+		}
+	}
+
+	if (!InitializeSockets())
+	{
+		printf("failed to initialize sockets\n");
+		return 1;
+	}
+	ReliableConnection connection(ProtocolId, TimeOut);
+	const int port = mode == Server ? ServerPort : ClientPort;
+	if (!connection.Start(port))
+	{
+		printf("could not start connection on port %d\n", port);
+		return 1;
+	}
+	if (mode == Client)
+		connection.Connect(address);
+	else
+		connection.Listen();
+	bool connected = false;
+	float sendAccumulator = 0.0f;
+	float statsAccumulator = 0.0f;
+	FlowControl flowControl;
+	while (true)
+	{
+
+		if (connection.IsConnected())
+			flowControl.Update(DeltaTime, connection.GetReliabilitySystem().GetRoundTripTime() * 1000.0f);
+		const float sendRate = flowControl.GetSendRate();
+
+		if (mode == Server && connected && !connection.IsConnected())
+		{
+			flowControl.Reset();
+			printf("reset flow control\n");
+			connected = false;
+		}
+		if (!connected && connection.IsConnected())
+		{
+			printf("client connected to server\n");
+			connected = true;
+		}
+
+		if (!connected && connection.ConnectFailed())
+		{
+			printf("connection failed\n");
+			break;
+		}
+
+		if (mode == Client && connected) {
+			printf("Enter filename to send: ");
+			if (scanf("%255s", filename) != 1) {
+				printf("Error: Invalid filename input!\n");
+				return 1;
+			}
+
+			FILE* file = fopen(filename, "rb");
+			if (!file) {
+				printf("Error: File not found! (%s)\n", filename);
+				return 1;
+			}
+			fclose(file);
+			Send_File_With_Md5(filename, connection);
+		}
+		if (mode == Server && connected) {
+
+
+			Receive_File_With_Md5(connection);
+		}
+
+
+
+		// send and receive packets
+		sendAccumulator += DeltaTime;
+		while (sendAccumulator > 1.0f / sendRate)
+		{
+			unsigned char packet[PacketSize];
+			char pack[1000] = "";
+			connection.SendPacket(packet, sizeof(packet));
+			n++;
+			sendAccumulator -= 1.0f / sendRate;
+		}
+		while (true)
+		{
+			unsigned char packet[256];
+			int bytes_read = connection.ReceivePacket(packet, sizeof(packet));
+			if (bytes_read == 0)
+				break;
+			printf("packets: %s ", packet);
+		}
+
+
+
+#ifdef SHOW_ACKS
+		unsigned int* acks = NULL;
+		int ack_count = 0;
+		connection.GetReliabilitySystem().GetAcks(&acks, ack_count);
+		if (ack_count > 0)
+		{
+			printf("acks: %d", acks[0]);
+			for (int i = 1; i < ack_count; ++i)
+				printf(",%d", acks[i]);
+			printf("\n");
+		}
+#endif
+		// update connection
+		connection.Update(DeltaTime);
+		// show connection stats
+		statsAccumulator += DeltaTime;
+		while (statsAccumulator >= 0.25f && connection.IsConnected())
+		{
+			float rtt = connection.GetReliabilitySystem().GetRoundTripTime();
+			unsigned int sent_packets = connection.GetReliabilitySystem().GetSentPackets();
+			unsigned int acked_packets = connection.GetReliabilitySystem().GetAckedPackets();
+			unsigned int lost_packets = connection.GetReliabilitySystem().GetLostPackets();
+			float sent_bandwidth = connection.GetReliabilitySystem().GetSentBandwidth();
+			float acked_bandwidth = connection.GetReliabilitySystem().GetAckedBandwidth();
+			printf("rtt %.1fms, sent %d, acked %d, lost %d (%.1f%%), sent bandwidth = %.1fkbps, acked bandwidth = %.1fkbps\n",
+				rtt * 1000.0f, sent_packets, acked_packets, lost_packets,
+				sent_packets > 0.0f ? (float)lost_packets / (float)sent_packets * 100.0f : 0.0f,
+				sent_bandwidth, acked_bandwidth);
+			statsAccumulator -= 0.25f;
+		}
+		net::wait(DeltaTime);
+	}
+	ShutdownSockets();
+	return 0;
+}
